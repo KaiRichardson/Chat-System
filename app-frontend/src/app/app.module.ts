8158d2a55f6f436b695a62ee4101026533fbd355
Kai Richardson <kairichardson2000@gmail.com>
2021-05-11 11:20:16 -0700

    chat framework installed

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app-frontend/src/app/chat-inbox/chat-inbox.component.ts
+++ app-frontend/src/app/chat-inbox/chat-inbox.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {io} from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 const SOCKET_ENDPOINT = 'localhost:3000';
 
 @Component({
   selector: 'app-chat-inbox',
   templateUrl: './chat-inbox.component.html',
-  styleUrls: ['./chat-inbox.component.scss']
+  styleUrls: ['./chat-inbox.component.scss'],
 })
 export class ChatInboxComponent implements OnInit {
   socket;
-  constructor() { }
+  message: string;
+  constructor() {}
 
   ngOnInit() {
     this.setupSocketConnection();
@@ -18,5 +19,27 @@ export class ChatInboxComponent implements OnInit {
 
   setupSocketConnection() {
     this.socket = io(SOCKET_ENDPOINT);
+    this.socket.on('message-broadcast', (data: string) => {
+      if (data) {
+        const element = document.createElement('li');
+        element.innerHTML = data;
+        element.style.background = 'white';
+        element.style.padding = '15px 30px';
+        element.style.margin = '10px';
+        document.getElementById('message-list').appendChild(element);
+      }
+    });
+  }
+
+  SendMessage() {
+    this.socket.emit('message', this.message);
+    const element = document.createElement('li');
+    element.innerHTML = this.message;
+    element.style.background = 'white';
+    element.style.padding = '15px 30px';
+    element.style.margin = '10px';
+    element.style.textAlign = 'right';
+    document.getElementById('message-list').appendChild(element);
+    this.message = '';
   }
 }
