8df5fb57f11125e72edb818f605785b0cafb9c87
Kai Richardson <kairichardson2000@gmail.com>
2021-05-11 10:51:41 -0700

    adding inbox comp

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatInboxComponent
   ],
   imports: [
     BrowserModule,
