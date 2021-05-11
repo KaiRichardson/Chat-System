8ed83dbabd9e8035c6c832a39575a7582f69e861
Kai Richardson <kairichardson2000@gmail.com>
2021-05-11 13:51:43 -0700

    adding Modules

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -1,22 +1,63 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
-import { FormsModule } from '@angular/forms';
+import { ChatService } from './chat.service';
+import { SocialLoginModule } from 'angularx-social-login';
+import {
+  AuthServiceConfig,
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  LinkedInLoginProvider,
+} from 'angularx-social-login';
+import { DataService } from './data.service';
+import { LoginComponent } from './login/login.component';
+import { SecondLoginComponent } from './second-login/second-login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider(
+      '663302841536-pqlvbekggnmb05301rr9g3r0rm0e44pb.apps.googleusercontent.com'
+    ),
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('524954454603212'),
+  },
+]);
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatInboxComponent
+    ChatInboxComponent,
+    LoginComponent,
+    SecondLoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SocialLoginModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    ChatService,
+    DataService,
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
