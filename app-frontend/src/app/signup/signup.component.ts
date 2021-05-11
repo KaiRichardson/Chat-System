e25fc0431490437b53dea9e82143bd5c43db7da2
Kai Richardson <kairichardson2000@gmail.com>
2021-05-11 15:55:21 -0700

    adding signup component

--- app-frontend/src/app/signup/signup.component.ts
+++ app-frontend/src/app/signup/signup.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  showDialogeBox = false;
+  constructor(private dataService: DataService, private router: Router) {}
+  register(frm: NgForm) {
+    const newUser = {
+      Name: frm.value.Name,
+      userName: frm.value.userName,
+      email: frm.value.email,
+      password: frm.value.password,
+    };
+    this.dataService.signUp(newUser).subscribe(
+      (res) => {
+        console.log('Account Created');
+        localStorage.setItem('username', res.username);
+        localStorage.setItem('name', res.name);
+        localStorage.setItem('email', res.email);
+        this.router.navigate(['/chat/', localStorage.getItem('username')]);
+      },
+      (err) => {
+        console.log('this is error', err);
+        this.showDialogeBox = true;
+      }
+    );
   }
-
+  ngOnInit() {}
 }
